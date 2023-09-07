8ba8d147abacc84cb70992aef5c8dfb78286edce
Davi Luna <Sdavi738@gmail.com>
2023-09-07 20:43:35 -0300

    feat:Created the header and handshake archive

--- packet.c
+++ packet.c
@@ -4,23 +4,24 @@
     Licensed under the MIT License.
 
     Abstract:
-        This file contains the implementation of the packet header.
+        This file contains the implementation of the packet header. Actually, my reference is the rfc
 
 */
 #include <stdint.h>
 #include <stddef.h> //for NULL
+#include "packet.h"
 
-struct Header{
-    uint8_t formHeader; //The form header must have 1 bit
-    uint8_t versionSpecific; //The form specific must have 7 bits
-    uint32_t *version;//The version must have 32 bits and initially(before connection it's null)
-    uint8_t DConnIDLen;//The Destination connection length must have 8 bits
-    uint16_t DConnID;//arbitrary value
-    uint16_t OConnIDLen;//The origin connection length must have 16 bits
-    uint8_t OConnID;//The  origin connection ID must have 8 bits
-    uint32_t Sversion;//Supported version
-    //there is also the version specific data but i didn't put it here for now
-};
+//struct Header{
+//    uint8_t formHeader; //The form header must have 1 bit
+//    uint8_t versionSpecific; //The form specific must have 7 bits
+//    uint32_t *version;//The version must have 32 bits and initially(before connection it's null)
+//    uint8_t DConnIDLen;//The Destination connection length must have 8 bits
+//    uint16_t DConnID;//arbitrary value
+//    uint16_t OConnIDLen;//The origin connection length must have 16 bits
+//    uint8_t OConnID;//The  origin connection ID must have 8 bits
+//    uint32_t Sversion;//Supported version
+//    //there is also the version specific data but i didn't put it here for now
+//};
 
 void longHeader(struct Header Header){
     Header.formHeader = 0x01;
@@ -39,5 +40,8 @@ void shortHeader(struct Header Header){//function to create a initial long heade
     Header.DConnID = 0x00;
 };
 
+
+
 void versionNegotiation(struct Header Header){
+
 }
---
+++ packet.h
@@ -0,0 +1,32 @@
+/*
+    Davi Souza de Luna
+
+    Licensed under the MIT License.
+
+    Abstract:
+        This file contains the header for packet functions.
+
+*/
+
+#ifndef packet_h_included
+#define packet_h_included
+
+typedef struct Header{
+    uint8_t formHeader; //The form header must have 1 bit
+    uint8_t versionSpecific; //The form specific must have 7 bits
+    uint32_t *version;//The version must have 32 bits and initially(before connection it's null)
+    uint8_t DConnIDLen;//The Destination connection length must have 8 bits
+    uint16_t DConnID;//arbitrary value
+    uint16_t OConnIDLen;//The origin connection length must have 16 bits
+    uint8_t OConnID;//The  origin connection ID must have 8 bits
+    uint32_t Sversion;//Supported version
+    //there is also the version specific data but i didn't put it here for now
+};
+
+void longHeader(struct Header Header){
+};
+
+void shortHeader(struct Header Header){//function to create a initial long header
+};
+
+#endif
