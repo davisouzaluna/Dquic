2746803f38c0f788ee4f3e72b9cccfede895ad84
Davi Luna <Sdavi738@gmail.com>
2024-01-29 15:23:45 -0300

    feat:completing the frame structure and creating a structure with the name frame

--- frame_struct.h
+++ frame_struct.h
@@ -68,3 +68,157 @@ typedef struct ACK_ECN_FRAME{
     int ecnSection;
 
     }ACK_ECN_FRAME;
+
+typedef struct RESET_STREAM_FRAME{
+    uint8_t type= RESET_STREAM;
+    int streamId;
+    int applicationErrorCode;
+    int finalSize;
+    }RESET_STREAM_FRAME;
+
+typedef struct STOP_SENDING_FRAME{
+    uint8_t type= STOP_SENDING;
+    int streamId;
+    int applicationErrorCode;
+    }STOP_SENDING_FRAME;
+
+typedef struct CRYPTO_FRAME{
+    uint8_t type= CRYPTO;
+    int offset;
+    int length;
+    int cryptoData;
+    }CRYPTO_FRAME;
+
+typedef struct NEW_TOKEN_FRAME{
+    uint8_t type= NEW_TOKEN;
+    int length;
+    int token;
+    }NEW_TOKEN_FRAME;
+
+typedef struct STREAM_FRAME{    
+    uint8_t type= STREAM;
+    int streamId;
+    int offset;
+    int length;
+    int streamData;
+    }STREAM_FRAME;
+
+typedef struct MAX_DATA_FRAME{
+    uint8_t type= MAX_DATA;
+    int maximumData;
+    }MAX_DATA_FRAME;
+
+typedef struct MAX_STREAM_DATA_FRAME{
+    uint8_t type= MAX_STREAM_DATA;
+    int streamId;
+    int maximumStreamData;
+    }MAX_STREAM_DATA_FRAME;
+
+//Bidirecional streams
+typedef struct MAX_STREAMS_BIDI_FRAME{
+    uint8_t type= MAX_STREAMS_BIDI;
+    int maximumStreamsBidi;
+    }MAX_STREAMS_BIDI_FRAME;
+
+//Unidirecional streams
+typedef struct MAX_STREAMS_UNI_FRAME{
+    uint8_t type= MAX_STREAMS_UNI;
+    int maximumStreamsUni;
+    }MAX_STREAMS_UNI_FRAME;
+
+
+typedef struct DATA_BLOCKED_FRAME{
+    uint8_t type= DATA_BLOCKED;
+    int dataLimit;
+    }DATA_BLOCKED_FRAME;
+
+typedef struct STREAM_DATA_BLOCKED_FRAME{
+    uint8_t type= STREAM_DATA_BLOCKED;
+    int streamId;
+    int dataLimit;
+    }STREAM_DATA_BLOCKED_FRAME;
+
+typedef struct STREAMS_BLOCKED_BIDI_FRAME{
+    uint8_t type= STREAMS_BLOCKED_BIDI;
+    int streamLimit;
+    }STREAMS_BLOCKED_BIDI_FRAME;
+
+typedef struct STREAMS_BLOCKED_UNI_FRAME{
+    uint8_t type= STREAMS_BLOCKED_UNI;
+    int streamLimit;
+    }STREAMS_BLOCKED_UNI_FRAME;
+
+typedef struct NEW_CONNECTION_ID_FRAME{
+    uint8_t type= NEW_CONNECTION_ID;
+    int sequenceNumber;
+    int connectionId;
+    int statelessResetToken;
+    }NEW_CONNECTION_ID_FRAME;
+
+typedef struct RETIRE_CONNECTION_ID_FRAME{
+    uint8_t type= RETIRE_CONNECTION_ID;
+    int sequenceNumber;
+    }RETIRE_CONNECTION_ID_FRAME;
+
+typedef struct PATH_CHALLENGE_FRAME{    
+    uint8_t type= PATH_CHALLENGE;
+    int data;
+    }PATH_CHALLENGE_FRAME;
+
+typedef struct PATH_RESPONSE_FRAME{
+    uint8_t type= PATH_RESPONSE;
+    int data;
+    }PATH_RESPONSE_FRAME;
+
+typedef struct CONNECTION_CLOSE_FRAME{
+    uint8_t type= CONNECTION_CLOSE;
+    int errorCode;
+    int frameType;
+    int reasonPhrase;
+    }CONNECTION_CLOSE_FRAME;
+
+typedef struct APPLICATION_CLOSE_FRAME{
+    uint8_t type= APPLICATION_CLOSE;
+    int errorCode;
+    int frameType;
+    int reasonPhrase;
+    }APPLICATION_CLOSE_FRAME;
+
+typedef struct HANDSHAKE_DONE_FRAME{
+    uint8_t type= HANDSHAKE_DONE;
+    }HANDSHAKE_DONE_FRAME;
+
+typedef struct FRAME{
+    uint8_t type;
+    int largestAcknowledged;
+    int ackDelay;
+    int ackRangeCount;
+    int firstAckRange;
+    int ecnCount;
+    int ecnSection;
+    int streamId;
+    int applicationErrorCode;
+    int finalSize;
+    int offset;
+    int length;
+    int cryptoData;
+    int maximumData;
+    int maximumStreamData;
+    int maximumStreamsBidi;
+    int maximumStreamsUni;
+    int dataLimit;
+    int streamLimit;
+    int sequenceNumber;
+    int connectionId;
+    int statelessResetToken;
+    int data;
+    int errorCode;
+    int frameType;
+    int reasonPhrase;
+    }FRAME;
+
+typedef struct FRAME_LIST{
+    FRAME frame;
+    struct FRAME_LIST *next;
+    }FRAME_LIST;
+
